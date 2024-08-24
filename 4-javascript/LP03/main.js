316047e3653533a761be59d1c77c02e203abc0a8
Rafael Carvalho <rafacien.rc@gmail.com>
2024-08-24 08:08:37 -0400

    Atualização do exercício LP03

--- 4-javascript/LP03/main.js
+++ 4-javascript/LP03/main.js
@@ -7,7 +7,7 @@ const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
 // setup objects
-const forms = ["Ball", "Square"];
+const forms = ["Ball", "Square", "Rectangle"];
 var objects = []
 
 var mainColor = parseInt(prompt("Escolha sua cor:\n1 - Vermelho\n2 - Verde\n3 - Azul\n"))
@@ -56,24 +56,36 @@ function randomObject() {
 }
 
 
-function Square(x, y, velX, velY, color, size) {
+function Poli(x, y, velX, velY, color, size, form) {
   this.x = x;
   this.y = y;
   this.velX = velX;
   this.velY = velY;
   this.color = color;
   this.size = size;
+  this.form = form;
+  if(this.form === "Rectangle") this.width = random(10, 20)
   this.invincibilityFrames = 0;
 }
 
-Square.prototype.draw = function () {
+Poli.prototype.draw = function () {
   ctx.beginPath();
   ctx.fillStyle = this.color;
-  ctx.fillRect(this.x, this.y, this.size, this.size);
+  switch (this.form) {
+    case "Ball":
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      break;
+    case "Square":
+      ctx.fillRect(this.x, this.y, this.size, this.size);
+      break;
+    case "Rectangle":
+      ctx.fillRect(this.x, this.y, this.size, this.width);
+      break;
+  }
   ctx.fill();
 };
 
-Square.prototype.update = function () {
+Poli.prototype.update = function () {
   if (this.invincibilityFrames > 0) this.invincibilityFrames -= 1;
   if (this.x + this.size >= width-5) {
     this.velX = -this.velX;
@@ -95,82 +107,7 @@ Square.prototype.update = function () {
   this.y += this.velY;
 };
 
-Square.prototype.collisionDetect = function () {
-  for (let j = 0; j < objects.length; j++) {
-    if (!(this === objects[j]) && (this.invincibilityFrames === 0 && objects[j].invincibilityFrames === 0)) {
-      const dx = this.x - objects[j].x;
-      const dy = this.y - objects[j].y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + objects[j].size) {
-        objects[j].color = this.color = randomRGB();
-        
-        this.invincibilityFrames = 7;
-        objects[j].invincibilityFrames = 7;
-        
-        if (Math.abs(objects[j].x - this.x) < 5 && Math.abs(this.velX + objects[j].velX) > Math.abs(this.velX)) {
-          menorX(this, objects[j]).velX = - this.velX;
-        }else if (Math.abs(objects[j].y - this.y) < 5 && Math.abs(this.velY + objects[j].velY) > Math.abs(this.velY)) {
-          menorY(this, objects[j]).velY = - this.velY;
-        }else if (Math.abs(objects[j].x - this.x) < 5) {
-          objects[j].velX = -objects[j].velX;
-          this.velX = - this.velX;
-        }else if (Math.abs(objects[j].y - this.y) < 5) {
-          objects[j].velY = -objects[j].velY;
-          this.velY = - this.velY;
-        }else {
-          objects[j].velX = -objects[j].velX;
-          this.velX = - this.velX;
-          objects[j].velY = -objects[j].velY;
-          this.velY = - this.velY;
-        }
-      }
-    }
-  }
-};
-
-// defining ball
-
-function Ball(x, y, velX, velY, color, size) {
-  this.x = x;
-  this.y = y;
-  this.velX = velX;
-  this.velY = velY;
-  this.color = color;
-  this.size = size;
-  this.invincibilityFrames = 0;
-}
-
-Ball.prototype.draw = function () {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
-};
-
-Ball.prototype.update = function () {
-  if (this.invincibilityFrames > 0) this.invincibilityFrames -= 1;
-  if (this.x + this.size >= width-5) {
-    this.velX = -this.velX;
-  }
-
-  if (this.x - this.size <= 0) {
-    this.velX = -this.velX;
-  }
-
-  if (this.y + this.size >= height-5) {
-    this.velY = -this.velY;
-  }
-
-  if (this.y - this.size <= 0) {
-    this.velY = -this.velY;
-  }
-
-  this.x += this.velX;
-  this.y += this.velY;
-};
-
-Ball.prototype.collisionDetect = function () {
+Poli.prototype.collisionDetect = function () {
   for (let j = 0; j < objects.length; j++) {
     if (!(this === objects[j]) && (this.invincibilityFrames === 0 && objects[j].invincibilityFrames === 0)) {
       const dx = this.x - objects[j].x;
@@ -208,31 +145,15 @@ Ball.prototype.collisionDetect = function () {
 
 while (objects.length < 25) {
   let size = random(10, 20);
-  let obj;
-
-  switch (randomObject()){
-    case "Ball":
-      obj = new Ball(
+  let obj = new Poli(
         random(0 + size, width - size),
         random(0 + size, height - size),
         random(-7, 7),
         random(-7, 7),
         randomRGB(),
         size,
+        randomObject(),
       );
-      break;
-    case "Square":
-      obj = new Square(
-        random(0 + size, width - size),
-        random(0 + size, height - size),
-        random(-7, 7),
-        random(-7, 7),
-        randomRGB(),
-        size,
-      );
-      break;
-
-  }
 
   objects.push(obj);
 }
