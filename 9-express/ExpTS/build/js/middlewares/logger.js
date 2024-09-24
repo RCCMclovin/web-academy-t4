bf90280045fadcc0d2e35a54d04eb8841cf79ef5
Rafael Castilho Carvalho <rafacien.rc@gmail.com>
2024-09-24 11:43:43 -0400

    Atualizar resolução de ExpTS para atualizar rota /lorem

--- 9-express/ExpTS/build/js/middlewares/logger.js
+++ 9-express/ExpTS/build/js/middlewares/logger.js
@@ -5,28 +5,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 function logger(path, type) {
-    if (type === "simples") {
-        return ((req, res, next) => {
-            fs_1.default.appendFile(path + "log.txt", `${new Date().toISOString()}, ${req.url}, ${req.method}\n`, (err) => {
+    if (type === 'simples') {
+        return (req, res, next) => {
+            fs_1.default.appendFile(path + 'log.txt', `${new Date().toISOString()}, ${req.url}, ${req.method}\n`, (err) => {
                 if (err)
                     throw err;
             });
             next();
-        });
+        };
     }
-    else if (type === "completo") {
-        return ((req, res, next) => {
-            fs_1.default.appendFile(path + "log.txt", `${new Date().toISOString()}, ${req.url}, ${req.method}, ${req.httpVersion}, ${req.get("User-Agent")}\n`, (err) => {
+    else if (type === 'completo') {
+        return (req, res, next) => {
+            fs_1.default.appendFile(path + 'log.txt', `${new Date().toISOString()}, ${req.url}, ${req.method}, ${req.httpVersion}, ${req.get('User-Agent')}\n`, (err) => {
                 if (err)
                     throw err;
             });
             next();
-        });
+        };
     }
     else {
-        return ((req, res, next) => {
+        return (req, res, next) => {
             next();
-        });
+        };
     }
 }
 exports.default = logger;
--- 9-express/ExpTS/build/js/router/router.js
+++ 9-express/ExpTS/build/js/router/router.js
@@ -5,68 +5,68 @@ const lorem_ipsum_1 = require("lorem-ipsum");
 const lorem = new lorem_ipsum_1.LoremIpsum({
     sentencesPerParagraph: {
         max: 15,
-        min: 4
+        min: 4,
     },
     wordsPerSentence: {
         max: 16,
-        min: 4
-    }
+        min: 4,
+    },
 });
-lorem.format = "html";
+lorem.format = 'html';
 const router = (0, express_1.Router)();
-router.get("/", (req, res) => {
-    res.send("Hello World!");
+router.get('/', (req, res) => {
+    res.send('Hello World!');
 });
-router.get("/about", (req, res) => {
-    const msg = "Mensagem sobre a página";
-    res.render("about", {
+router.get('/about', (req, res) => {
+    const msg = 'Mensagem sobre a página';
+    res.render('about', {
         layout: false,
         msg,
     });
 });
-router.get("/lorem/:num", (req, res) => {
+router.get('/lorem/:num', (req, res) => {
     const { num } = req.params;
     res.send(lorem.generateParagraphs(Number(num)));
 });
-router.get("/hb1", (req, res) => {
-    res.render("hb1", {
-        mensagem: "Olá, você está aprendendo Express + HBS!",
+router.get('/hb1', (req, res) => {
+    res.render('hb1', {
+        mensagem: 'Olá, você está aprendendo Express + HBS!',
         layout: false,
     });
 });
-router.get("/hb2", (req, res) => {
-    res.render("hb2", {
+router.get('/hb2', (req, res) => {
+    res.render('hb2', {
         poweredbyNodejs: true,
-        name: "Express",
-        type: "Framework",
+        name: 'Express',
+        type: 'Framework',
         layout: false,
     });
 });
-router.get("/hb3", (req, res) => {
+router.get('/hb3', (req, res) => {
     const profs = [
-        { nome: "David Fernandes", sala: 1234 },
-        { nome: "Horácio Fernandes", sala: 1235 },
-        { nome: "Edleno Moura", sala: 1236 },
-        { nome: "Elaine harada", sala: 1237 },
+        { nome: 'David Fernandes', sala: 1234 },
+        { nome: 'Horácio Fernandes', sala: 1235 },
+        { nome: 'Edleno Moura', sala: 1236 },
+        { nome: 'Elaine harada', sala: 1237 },
     ];
-    res.render("hb3", {
+    res.render('hb3', {
         profs,
         layout: false,
     });
 });
-router.get("/profs", (req, res) => {
+router.get('/profs', (req, res) => {
     const profs = [
-        { nome: "David Fernandes", sala: 1234 },
-        { nome: "Horácio Fernandes", sala: 1235 },
-        { nome: "Edleno Moura", sala: 1236 },
-        { nome: "Elaine harada", sala: 1237 },
+        { nome: 'David Fernandes', sala: 1234 },
+        { nome: 'Horácio Fernandes', sala: 1235 },
+        { nome: 'Edleno Moura', sala: 1236 },
+        { nome: 'Elaine harada', sala: 1237 },
     ];
-    res.render("profs", {
+    res.render('profs', {
         profs,
         layout: false,
     });
 });
-router.get("/hb4", (req, res) => {
+router.get('/hb4', (req, res) => {
     const techs = [
         { name: 'Express', type: 'Framework', poweredByNodejs: true },
         { name: 'Laravel', type: 'Framework', poweredByNodejs: false },
@@ -76,17 +76,17 @@ router.get("/hb4", (req, res) => {
         { name: 'Docker', type: 'Virtualization', poweredByNodejs: false },
         { name: 'Sequelize', type: 'ORM tool', poweredByNodejs: true },
     ];
-    res.render("hb4", {
+    res.render('hb4', {
         techs,
         layout: false,
     });
 });
-router.get("/lorem", (req, res) => {
+router.get('/lorem', (req, res) => {
     const { num } = req.query;
-    let paragraphs = "";
+    let paragraphs = '';
     if (num)
         paragraphs = lorem.generateParagraphs(Number(num));
-    res.render("lorem", {
+    res.render('lorem', {
         paragraphs,
         layout: false,
     });
