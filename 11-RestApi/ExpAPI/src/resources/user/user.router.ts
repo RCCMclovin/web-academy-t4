df3ce68bbede2db3b45b804aa70abd48a498dccc
Rafael Castilho Carvalho <rafacien.rc@gmail.com>
2024-10-30 18:17:47 -0400

    Resolução do exercício ExpAPI de Cookies e Sessões

--- 11-RestApi/ExpAPI/src/resources/user/user.router.ts
+++ 11-RestApi/ExpAPI/src/resources/user/user.router.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import userController from './user.controller';
 import { userSchema } from './user.schema';
 import { validate } from '../../midlewares/validate';
+import isAdmin from '../../midlewares/isAdmin';
 
 const router = Router();
 
-router.get('/', userController.index);
-router.post('/', validate(userSchema), userController.create);
+router.get('/', isAdmin, userController.index);
+router.post('/', isAdmin, validate(userSchema), userController.create);
 router.get('/:id', userController.read);
 router.put('/:id', validate(userSchema), userController.update);
-router.delete('/:id', userController.remove);
+router.delete('/:id', isAdmin, userController.remove);
 
 export default router;
