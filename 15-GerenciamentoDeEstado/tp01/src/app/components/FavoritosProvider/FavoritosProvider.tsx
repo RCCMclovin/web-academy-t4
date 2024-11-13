425b00881ae2beb57163912b3591343ba6eb75bd
Rafael Castilho Carvalho <rafacien.rc@gmail.com>
2024-11-12 20:39:35 -0400

    Resolução TP01 Gerenciamento de Estado

---
+++ 15-GerenciamentoDeEstado/tp01/src/app/components/FavoritosProvider/FavoritosProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { createContext, useState } from "react";
+
+interface IFavoritosContext{
+    favoritos: Produto[];
+    setFavoritos: React.Dispatch<React.SetStateAction<Produto[]>>;
+}
+
+interface FavoritosProviderProps{
+    children: React.ReactNode;
+}
+  
+export const FavoritosContext = createContext<IFavoritosContext>({
+    favoritos: [],
+    setFavoritos: () => { },
+});
+
+const FavoritosProvider = ({ children }: FavoritosProviderProps) => {
+  const [favoritos, setFavoritos] = useState<Produto[]>([]);
+
+  const value = { favoritos, setFavoritos };
+
+    return (
+        <FavoritosContext.Provider value={value}>
+            {children}
+        </FavoritosContext.Provider>
+    )
+}
+
+export default FavoritosProvider;
---
+++ 15-GerenciamentoDeEstado/tp01/src/app/favoritos/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import ListagemFavoritos from "../components/ListagemFavoritos/ListagemFavoritos";
+import { useState } from "react";
+
+export default function Favoritos() {
+  const [favoritos, setFavoritos] = useState<Produto[] | []>([]);
+
+  return (
+    <main>
+      <div className="container p-5">
+        <ListagemFavoritos/>
+      </div>
+    </main>
+  );
+}
